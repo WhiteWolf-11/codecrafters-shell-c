cbc21972b588e7ecd88b7eaba771b8a8e47d5d85
Devanshu <devanshuyelurkar@gmail.com>
2025-12-24 17:18:22 +0530

    added type command

--- src/main.c
+++ src/main.c
@@ -5,6 +5,8 @@
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
+  char *bltin[50]= {"exit", "echo", "type"};
+  
 
 while(1) {  
   // TODO: Uncomment the code below to pass the first stage
@@ -15,13 +17,29 @@ while(1) {
   fgets(input, 100, stdin);
 
   // Remove the trailing newline
-  input[strlen(input) - 1] = '\0';
+  input[strcspn(input, "\n")] = '\0';
   if (strcmp(input, "exit") == 0){
     break;
   }
   else if (strncmp(input, "echo",4) == 0){
     printf("%s\n", input + 5);
   }
+  else if (strncmp(input, "type", 4) == 0){
+    int flag = 0; 
+    for(int i=0; i < 3; i++){
+    
+      if (strcmp(input+5, bltin[i]) == 0 ){
+        flag = 1;
+      }
+    };
+    if (flag == 1){
+      printf("%s is a shell builtin\n", input +5);
+    }
+      else {
+        printf("%s: not found\n", input +5);
+      }
+    }
+
   else{
   printf("%s: command not found\n", input);
   }
