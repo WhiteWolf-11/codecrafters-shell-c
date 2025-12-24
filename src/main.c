1e1d6f938b1ae87ea66766bbf1fdeb6f00e3a514
Devanshu <devanshuyelurkar@gmail.com>
2025-12-24 13:15:16 +0530

    added loop

--- src/main.c
+++ src/main.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
+while(1) {  
   // TODO: Uncomment the code below to pass the first stage
-  while (1) {
-   printf("$ ");
+  printf("$ ");
 
-   scanf("%s", argv[0]);
-   printf("%s: command not found\n", argv[0]);
-  }
-  return 0;
+  // Wait for user input
+  char input[100];
+  fgets(input, 100, stdin);
+
+  // Remove the trailing newline
+  input[strlen(input) - 1] = '\0';
+  printf("%s: command not found\n", input);
+}
+return 0;
 }
