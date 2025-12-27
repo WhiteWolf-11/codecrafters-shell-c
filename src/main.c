f6846c59657ee3024cdd4f21e2a9b4dd744159be
Devanshu <devanshuyelurkar@gmail.com>
2025-12-27 12:31:50 +0530

    locate exec files

--- src/main.c
+++ src/main.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
   char *bltin[50]= {"exit", "echo", "type"};
+ 
   
 
 while(1) {  
@@ -25,6 +28,7 @@ while(1) {
     printf("%s\n", input + 5);
   }
   else if (strncmp(input, "type", 4) == 0){
+    char *command = input + 5;
     int flag = 0; 
     for(int i=0; i < 3; i++){
     
@@ -33,12 +37,31 @@ while(1) {
       }
     };
     if (flag == 1){
-      printf("%s is a shell builtin\n", input +5);
+      printf("%s is a shell builtin\n", command);
     }
       else {
-        printf("%s: not found\n", input +5);
+        char *path = getenv("PATH");
+        char *path_copy = strdup(path);
+        char *dir = strtok(path_copy, ":");
+        char file_path[200];
+        while(dir != NULL){
+
+          snprintf(file_path, sizeof(file_path), "%s/%s", dir, command);
+          if(access(file_path, X_OK) == 0){
+            flag = 1;
+            break;
+          }
+           dir = strtok(NULL, ":");
+        }
+        free(path_copy);
+        if (flag == 1){
+          printf("%s is %s\n", command, file_path);
+        }
+        else {
+          printf("%s: command not found\n", command);
       }
     }
+  }
 
   else{
   printf("%s: command not found\n", input);
