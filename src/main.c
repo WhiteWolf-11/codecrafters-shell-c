d445b45ee689f1e9c3024efe0a76e714ef67c12f
Devanshu <devanshuyelurkar@gmail.com>
2025-12-30 13:05:40 +0530

    fixed error4

--- src/main.c
+++ src/main.c
@@ -5,22 +5,22 @@
 #include <sys/wait.h>
 
 
-char *file_path(char *command,char *res, int *flag){
-char *path = getenv("PATH");
- char *path_copy = strdup(path);
- char *dir = strtok(path_copy, ":");
- while(dir != NULL){
-
- snprintf(res,1024 , "%s/%s", dir, command);
-if(access(res, X_OK) == 0){
-  *flag = 1;
-  break;
+char *file_path(char *command,char *res, int *flag) {
+  char *path = getenv("PATH");
+  char *path_copy = strdup(path);
+  char *dir = strtok(path_copy, ":");
+  while (dir != NULL) {
+    snprintf(res, 1024, "%s/%s", dir, command);
+    if (access(res, X_OK) == 0) {
+      *flag = 1;
+      break;
+    }
+    dir = strtok(NULL, ":");
   }
-dir = strtok(NULL, ":");
-}
-    free(path_copy);
-    (*flag == 1) ? res : NULL;
-    return res;
+
+  free(path_copy);
+  (*flag == 1) ? res : NULL;
+  return res;
 }
 
 
@@ -29,88 +29,79 @@ int main() {
   setbuf(stdout, NULL);
   char *argv[20];
   char res[1024];
-  char *bltin[50]= {"exit", "echo", "type"};
- 
-while(1) {  
-  // TODO: Uncomment the code below to pass the first stage
-  printf("$ ");
+  char *bltin[50] = {"exit", "echo", "type"};
 
+  while (1) {
+    // TODO: Uncomment the code below to pass the first stage
+    printf("$ ");
 
-  // Wait for user input
-  char input[100];
-  if (fgets(input, 100, stdin) == NULL) break;
-  int i = 0;
+    // Wait for user input
+    char input[100];
+    if (fgets(input, 100, stdin) == NULL)
+      break;
+    int i = 0;
 
+    // Remove the trailing newline
+    input[strcspn(input, "\n")] = '\0';
+    char *token = strtok(input, " ");
 
-  // Remove the trailing newline
-  input[strcspn(input, "\n")] = '\0';
-  char *token = strtok(input, " ");
-
-  while(token != NULL && i < 19){
-    argv[i++] = token;
-    token = strtok(NULL, " ");
-  }
-  argv[i] = NULL;
-  char *command = argv[1];
-  if(argv[0]== NULL) continue;
-
-  if (strcmp(argv[0], "exit") == 0){
-    break;
-  }
-  else if (strncmp(argv[0], "echo",4) == 0){
-    printf("%s\n", input + 5);
-  }
-  else if (strncmp(argv[0], "type", 4) == 0){
-    
-    int flag = 0; 
-    for(int i=0; i < 3; i++){
-    
-      if (strcmp(argv[1], bltin[i]) == 0 ){
-        flag = 1;
-      }
-    };
-    if (flag == 1){
-      printf("%s is a shell builtin\n", command);
+    while (token != NULL && i < 19) {
+      argv[i++] = token;
+      token = strtok(NULL, " ");
     }
-      else {
-        char resolved_path[1024];
-        file_path(command,res, &flag);
-        if (flag == 1){
-          printf("%s is %s\n", command, res);
+    argv[i] = NULL;
+    char *command = argv[0];
+    if (argv[0] == NULL)
+      continue;
+
+    if (strcmp(argv[0], "exit") == 0) {
+      break;
+    } else if (strncmp(argv[0], "echo", 4) == 0) {
+      printf("%s\n", argv[1]);
+    } else if (strncmp(argv[0], "type", 4) == 0) {
+      int flag = 0;
+      for (int i = 0; i < 3; i++) {
+        if (strcmp(argv[1], bltin[i]) == 0) {
+          flag = 1;
         }
-        else {
+      };
+      if (flag == 1) {
+        printf("%s is a shell builtin\n", command);
+      } else {
+        char resolved_path[1024];
+        file_path(command, res, &flag);
+        if (flag == 1) {
+          printf("%s is %s\n", command, res);
+        } else {
           printf("%s: not found\n", command);
+        }
       }
-    }
-  }
+    } else {
+      int found_executable = 0;
+      // Search for the command in PATH
+      file_path(argv[0], res, &found_executable);
 
- else {
-    int found_executable = 0;
-    // Search for the command in PATH
-    file_path(argv[0], res, &found_executable);
-
-    if (found_executable == 1) {
+      if (found_executable == 1) {
         pid_t pid = fork();
         if (pid == 0) {
-            // Child process: execute the found path with the original argv
-            if (execv(res, (char * const*)argv) == -1) {
-                perror("execv");
-                exit(1);
-            }
+          // Child process: execute the found path with the original argv
+          if (execv(res, (char * const*)argv) == -1) {
+            perror("execv");
+            exit(1);
+          }
         } else if (pid < 0) {
-            perror("fork");
+          perror("fork");
         } else {
-            // Parent process: wait for the command to finish
-            wait(NULL);
+          // Parent process: wait for the command to finish
+          wait(NULL);
         }
-    } else {
+      } else {
         // If not found, print ONLY the command name followed by : command not found
         // The tester is very sensitive to spaces and extra characters!
         printf("%s: command not found\n", argv[0]);
-     }
- }
-    
-}
+      }
+    }
+  }
 
-return 0;
+  return 0;
 }
